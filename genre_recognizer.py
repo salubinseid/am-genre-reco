8a994ee5e819387fce4950cdba544c7d08c2f84e
Salahadin Seid <salubinseid@gmail.com>
2017-09-26 16:06:43 +0300

    Update genre_recognizer.py

--- genre_recognizer.py
+++ genre_recognizer.py
@@ -1,7 +1,1 @@
-from common import load_track, get_layer_output_function 
-import numpy as np
-from keras.layers import Input 
-from keras.models import model_from_yaml, Model 
-from keras import backend as K
-
-class GenereRecognizaer():
+# A code which load a model and made a prediction 
